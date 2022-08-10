f58fa981ed2b0aee50d6defafad6beccd718d973
galal10 <galalnasser5@gmail.com>
2022-08-10 17:02:25 +0200

    Platform_types.h

---
+++ unit3/Embedded_C.lesson1/platform_types.h
@@ -0,0 +1,30 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef unsigned char 			uint8_t;
+typedef signed char 			sint8_t;
+typedef unsigned short 			uint16_t;
+typedef signed char 			sint16_t;
+typedef unsigned int 			uint32_t;
+typedef signed int  			sint32_t;
+typedef unsigned long long int	uint64_t;
+typedef signed long long int 	sint64_t;
+
+
+typedef float			float32_t;
+typedef double			float64_t;
+
+
+typedef volatile uint8_t 	vuint8_t;
+typedef volatile sint8_t 	vsint8_t;
+typedef volatile uint16_t	vuint16_t;
+typedef volatile sint16_t	vsint16_t;
+typedef volatile uint32_t	vuint32_t;
+typedef volatile sint32_t	vsint32_t;
+typedef volatile uint64_t	vuint64_t;
+typedef volatile sint64_t	vsint64_t;
+typedef volatile float32_t	vfloat32_t;
+typedef volatile float64_t	vfloat64_t;
+
+
+#endif /* TYPES_H_ */
