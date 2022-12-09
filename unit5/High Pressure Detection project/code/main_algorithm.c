d102d04fb7f289a688f87c9725d1fcc2ad669327
Galal <galalnasser5@gmail.com>
2022-12-09 16:57:44 +0200

    implement main algorithm

---
+++ unit5/High Pressure Detection project/code/main_algorithm.c
@@ -0,0 +1,15 @@
+#include "main_algorithm.h"
+
+static int P_val;
+void (*P_main_ptr)();
+
+void waiting()
+{
+    P_val = getPressureVal();
+    (P_val > THERSHOLD) ? (P_main_ptr = High_pressure_detected) : (P_main_ptr = waiting);
+}
+
+void High_pressure_detected()
+{
+    Start_alarm();
+}
