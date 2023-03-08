aebacdcaa8436945c92a6860762e1838a1b79ade
Galal <67324703+galal10@users.noreply.github.com>
2023-03-08 22:56:54 +0200

    adding clocks registers

--- unit6/lesson3/lab1/lab1.c
+++ unit6/lesson3/lab1/lab1.c
@@ -28,11 +28,23 @@
 #define GPIOA_CRH 	*(vuint32_t *)(GPIOA_BASE+0x04)
 #define GPIOA_ODR	*(vuint32_t *)(GPIOA_BASE+0x0C)
 
+#define RCC_BASE	0x40021000
+#define RCC_APB2ENR	*(vuint32_t*)(RCC_BASE+0x18)
+
+
 //bit field
 #define GPIOA13  	 13
-
+#define IOPAEN		 2
 int main(void)
 {
+	/*
+	Bit 2 IOPAEN: IO port A clock enable
+	Set and cleared by software.
+	0: IO port A clock disabled
+	1: IO port A clock enabled
+	*/
+	RCC_APB2ENR |=1<<IOPAEN;
+
 	GPIOA_CRH &=0xff0fffff;
 	GPIOA_CRH |=0x00200000;
     /* Loop forever */
