841f50d9f0202b61ebe2b04c1b0179f0d79c6a17
Galal <67324703+galal10@users.noreply.github.com>
2023-08-27 16:03:55 +0300

    main code

---
+++ unit7/4.EXTI_driver/Section/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 27, 2023
+ *      Author: galal
+ */
+
+#include "MCAL/MDIO/MDIO_Interface.h"
+#include "HAL/HCLCD/HCLCD_Interface.h"
+
+int main(void)
+{
+	while(1)
+	{
+
+	}
+	return 0;
+}
