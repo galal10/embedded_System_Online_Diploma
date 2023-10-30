580cf4a6c211003c82884a3c5460ae71a543b4c4
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-10-30 19:40:52 +0200

    Send string function
    Synchronous mode

--- unit8/1-USART/Atmega32/Services/SUSART/SUSART.c
+++ unit8/1-USART/Atmega32/Services/SUSART/SUSART.c
@@ -7,3 +7,11 @@
 
 #include "../../MCAL/MUART/MUSART_Interface.h"
 #include "SUSART.h"
+/******************************* Synchronous *******************************/
+void SUSART_Vid_SendString(u8* P_u8_str)
+{
+	while(*P_u8_str)
+	{
+		MUSART_Vid_SendData(*P_u8_str++);
+	}
+}
