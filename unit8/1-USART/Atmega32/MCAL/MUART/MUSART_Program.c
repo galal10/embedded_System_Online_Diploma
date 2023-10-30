e62e8e2b3132a3a068b7ade90bd4f89a6bb37248
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-10-30 19:33:37 +0200

    UART implementation file

---
+++ unit8/1-USART/Atmega32/MCAL/MUART/MUSART_Program.c
@@ -0,0 +1,12 @@
+/*
+ * MUSART_Program.c
+ *
+ *  Created on: Oct 22, 2023
+ *      Author: galal
+ */
+#include "../../LIB/LBITS.h"
+#include "../../LIB/LSTD_TYPES.h"
+#include "MUSART_Interface.h"
+#include "MUSART_Config.h"
+#include "MUSART_Private.h"
+
