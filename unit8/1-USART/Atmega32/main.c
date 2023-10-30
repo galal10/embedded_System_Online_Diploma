2b41deb9928be3a09685f3eecee6b519513f5e6f
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-10-30 19:45:51 +0200

    include files

---
+++ unit8/1-USART/Atmega32/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 1, 2023
+ *      Author: galal
+ */
+
+#include "MCAL/MGIE/MGIE_Interface.h"
+#include "MCAL/MUART/MUSART_Interface.h"
+#include "Services/SUSART/SUSART.h"
+#include "HAL/HCLCD/HCLCD_Interface.h"
+#include "util/delay.h"
+
+
