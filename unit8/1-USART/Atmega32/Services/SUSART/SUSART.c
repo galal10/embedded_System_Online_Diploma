0f4b6609fa303efe157314c79297f8c4479b3dbc
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-10-30 19:41:14 +0200

    Receive String
    Synchronous mode

--- unit8/1-USART/Atmega32/Services/SUSART/SUSART.c
+++ unit8/1-USART/Atmega32/Services/SUSART/SUSART.c
@@ -15,3 +15,20 @@ void SUSART_Vid_SendString(u8* P_u8_str)
 		MUSART_Vid_SendData(*P_u8_str++);
 	}
 }
+
+void SUSART_Vid_ReceiveString(u8* P_u8_buffer)
+{
+	u8 index = 0;
+
+	P_u8_buffer[index] = MUSART_u8_ReceiveData();
+
+	/* ascii code of Enter */
+	while(P_u8_buffer[index] != 0x0d)
+	{
+		index++;
+		P_u8_buffer[index] = MUSART_u8_ReceiveData();
+	}
+
+	P_u8_buffer[index] = '\0';
+}
+
