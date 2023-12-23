b0361d6d4ef1dd0337fe80323d588a24cb6c32aa
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-12-24 00:35:01 +0200

    ADC Channels Options

---
+++ unit9/2-ADC/MADC/MADC_Interface.h
@@ -0,0 +1,22 @@
+/*
+ * MADC_Interface.h
+ *
+ *  Created on: Dec 23, 2023
+ *      Author: galal
+ */
+
+#ifndef MADC_INTERFACE_H_
+#define MADC_INTERFACE_H_
+
+#include "../../LIB/LSTD_TYPES.h"
+
+/* ADC Channels Options */
+#define CHANNEL_0		0
+#define CHANNEL_1		1
+#define CHANNEL_2		2
+#define CHANNEL_3		3
+#define CHANNEL_4		4
+#define CHANNEL_5		5
+#define CHANNEL_6		6
+#define CHANNEL_7		7
+
