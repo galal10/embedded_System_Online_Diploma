452577b517037b8d6048714ab451180223f181b0
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-12-24 00:34:38 +0200

    ADC voltage reference Options

--- unit9/2-ADC/MADC/MADC_Config.h
+++ unit9/2-ADC/MADC/MADC_Config.h
@@ -1,3 +1,22 @@
+/*
+ * MADC_Config.h
+ *
+ *  Created on: Dec 23, 2023
+ *      Author: galal
+ */
+
+#ifndef MADC_CONFIG_H_
+#define MADC_CONFIG_H_
+
+
+/* ADC voltage reference Options:
+ * 1- Vref_turned_off
+ * 2- AVCC_REF_VOLT
+ * 3- _2and_Half_V_REF_VOLT
+ */
+#define ADC_REF_VOLT		AVCC_REF_VOLT
+
+
 /* ADC PreScaler Options:
  * 1- ADC_2_PRESCALER
  * 2- ADC_4_PRESCALER
