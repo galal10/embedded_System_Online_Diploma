b8885d398f256c3c88258f636863d0f4a22afcbb
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-12-24 00:38:11 +0200

    ADC init function

---
+++ unit9/2-ADC/MADC/MADC_Program.c
@@ -0,0 +1,16 @@
+/*
+ * MADC_Program.c
+ *
+ *  Created on: Dec 23, 2023
+ *      Author: galal
+ */
+
+#include "../../LIB/LBITS.h"
+#include "../../LIB/LSTD_TYPES.h"
+#include "MADC_Private.h"
+#include "MADC_Config.h"
+
+/* ADC Initialization */
+void MADC_Vid_Init(void)
+{
+	/* Cleared voltage reference bits */
