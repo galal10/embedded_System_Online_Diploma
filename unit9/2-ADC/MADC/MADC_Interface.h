b668bf902760fd4c014a0481a33bf236455ab153
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-12-24 00:35:19 +0200

    prototype of functions

--- unit9/2-ADC/MADC/MADC_Interface.h
+++ unit9/2-ADC/MADC/MADC_Interface.h
@@ -20,3 +20,9 @@
 #define CHANNEL_6		6
 #define CHANNEL_7		7
 
+/* ADC Initialization */
+void MADC_Vid_Init(void);
+/* ADC Start Conversion ==> Polling, Return ADC value */
+u16 MADC_u16_StartConversion(u8 Copy_u8_Channel);
+
+#endif /* MADC_INTERFACE_H_ */
