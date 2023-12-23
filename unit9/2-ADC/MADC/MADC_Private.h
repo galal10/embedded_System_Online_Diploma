47a95c633be8223a43febd0375fd3701aad0fabf
Galal Nasser <67324703+galal10@users.noreply.github.com>
2023-12-24 00:36:04 +0200

    ADC Control and Status Register A

--- unit9/2-ADC/MADC/MADC_Private.h
+++ unit9/2-ADC/MADC/MADC_Private.h
@@ -20,3 +20,15 @@
 #define MUX1 			1
 #define MUX0			0
 
+/* ADC Control and Status Register A */
+#define ADCSRA			*((volatile u8*)0x26)
+/* ADCSRA Bits */
+#define ADEN 			7
+#define ADSC 			6
+#define ADATE 			5
+#define ADIF 			4
+#define ADIE 			3
+#define ADPS2 			2
+#define ADPS1 			1
+#define ADPS0			0
+
